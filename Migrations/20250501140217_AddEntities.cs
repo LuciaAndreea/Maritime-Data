3e91a48572f9df9304e54fe5b1c5fb9d3d2665e1
Lucia <luciaandreeaursu@yahoo.com>
2025-05-01 18:21:54 +0300

    added packages

---
+++ Migrations/20250501140217_AddEntities.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MaritimeData.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CountriesVisited",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CountryId = table.Column<int>(type: "integer", nullable: false),
+                    VisitDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CountriesVisited", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ships",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    MaxSpeed = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ships", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voyages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    VoyageData = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ShipId = table.Column<int>(type: "integer", nullable: false),
+                    DeparturePortName = table.Column<string>(type: "text", nullable: false),
+                    ArrivalPortName = table.Column<string>(type: "text", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voyages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CountriesVisited");
+
+            migrationBuilder.DropTable(
+                name: "Ports");
+
+            migrationBuilder.DropTable(
+                name: "Ships");
+
+            migrationBuilder.DropTable(
+                name: "Voyages");
+        }
+    }
+}
--- obj/Debug/net9.0/MaritimeData.AssemblyInfo.cs
+++ obj/Debug/net9.0/MaritimeData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaritimeData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023e4dd1cc566627143b37c31393e4cbb13ee8e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f460157a5ee9d361cd406d0667f764053ffc92")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaritimeData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaritimeData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
