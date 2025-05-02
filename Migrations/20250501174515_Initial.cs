6f47526c759b0536379c35892a5f7dcf0054fad4
Lucia <luciaandreeaursu@yahoo.com>
2025-05-02 09:11:24 +0300

    packages

---
+++ Migrations/20250501174515_Initial.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaritimeData.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Ships",
+                table: "Ships");
+
+            migrationBuilder.DropColumn(
+                name: "ArrivalPortName",
+                table: "Voyages");
+
+            migrationBuilder.RenameTable(
+                name: "Ships",
+                newName: "ships");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "ships",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "MaxSpeed",
+                table: "ships",
+                newName: "maxspeed");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "ships",
+                newName: "id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeparturePortName",
+                table: "Voyages",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArrivalPortNames",
+                table: "Voyages",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ships",
+                table: "ships",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ships",
+                table: "ships");
+
+            migrationBuilder.DropColumn(
+                name: "ArrivalPortNames",
+                table: "Voyages");
+
+            migrationBuilder.RenameTable(
+                name: "ships",
+                newName: "Ships");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Ships",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "maxspeed",
+                table: "Ships",
+                newName: "MaxSpeed");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Ships",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeparturePortName",
+                table: "Voyages",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArrivalPortName",
+                table: "Voyages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Ships",
+                table: "Ships",
+                column: "Id");
+        }
+    }
+}
--- obj/Debug/net9.0/MaritimeData.AssemblyInfo.cs
+++ obj/Debug/net9.0/MaritimeData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaritimeData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f460157a5ee9d361cd406d0667f764053ffc92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e91a48572f9df9304e54fe5b1c5fb9d3d2665e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaritimeData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaritimeData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
